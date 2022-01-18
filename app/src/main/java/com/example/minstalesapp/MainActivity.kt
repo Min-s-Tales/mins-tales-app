b4564b02da4e272a34199ca40138d70ea1657c93
Jérémy Poisson <89804917+JeremyPoisson2B@users.noreply.github.com>
2022-01-18 12:37:27 +0100

    Navigation works

--- app/src/main/java/com/example/minstalesapp/MainActivity.kt
+++ app/src/main/java/com/example/minstalesapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.minstalesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.minstales.ListStoryActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playButton.setOnClickListener {
+            startActivity(Intent(this, ListStoryActivity::class.java))
+        }
     }
 }
