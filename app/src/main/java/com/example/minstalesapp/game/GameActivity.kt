b6ee41db84f27f625f157419010fd67f788cd23c
Rosstail <rosstail_zegeek@hotmail.fr>
2022-03-10 20:36:39 +0100

    Setup a basic check + clean code

--- app/src/main/java/com/example/minstalesapp/game/GameActivity.kt
+++ app/src/main/java/com/example/minstalesapp/game/GameActivity.kt
@@ -3,6 +3,7 @@ package com.example.minstalesapp.game
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.util.Log
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
@@ -15,8 +16,9 @@ open class GameActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGameBinding
 
-    private var TAG = "GameActivity"
+    private val TAG = "[GameActivity]"
     private val model: GameActivityViewModel by viewModels()
+    private var text = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,49 +26,31 @@ open class GameActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        //setContentView(R.layout.activity_test1);
-        //setContentView(R.layout.activity_test1);
         val launcher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ) { result ->
             if (result.resultCode == RESULT_OK && result.data != null) {
                 val data = result.data
-                binding.tvText.setText(data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)!![0])
+                text = data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)!![0]
+                binding.tvText.setText(text)
+
+                if (text.lowercase().contains("bonjour")) {
+                    Log.i(TAG, "onCreate: BONJOUR PASSÉ AVEC SUCCES")
+                }
             }
         }
 
-        binding.recordButton.setOnClickListener { view -> run {
-            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-            intent.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-            )
-            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Start speaking")
+        binding.recordButton.setOnClickListener { view ->
+            run {
+                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                intent.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                )
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "$TAG Start speaking")
 
-            launcher.launch(intent)
+                launcher.launch(intent)
+            }
         }
-            //recognitionCustom.launch(5)
-        }
-    }
-
-    /*
-    private val recognitionCustom = registerForActivityResult(RecognitionContract()) { result ->
-        binding.tvText.setText(result)
-    }
-     */
-}
-/*
-class RecognitionContract : ActivityResultContract<Int, String>() {
-    override fun createIntent(context: Context, input: Int): Intent {
-        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, Locale.getDefault())
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to Text")
-        return intent
-    }
-
-
-    override fun parseResult(resultCode: Int, intent: Intent?): String {
-        return intent?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0) ?: ""
     }
 }
-*/
