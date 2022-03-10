815d8bf7be0489fdcb14850facbdedcff810bcb3
Rosstail <rosstail_zegeek@hotmail.fr>
2022-03-10 11:15:11 +0100

    Removed useless files

--- app/src/main/java/com/example/minstalesapp/MainActivity.kt
+++ app/src/main/java/com/example/minstalesapp/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.minstalesapp.game.GameActivity
-import com.example.minstalesapp.game.TestActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
