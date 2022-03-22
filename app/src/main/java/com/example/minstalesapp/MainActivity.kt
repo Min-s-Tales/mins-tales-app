a7d2e581e72a6d6b8b4a1202edecfebebb523410
Jérémy Poisson <89804917+JeremyPoisson2B@users.noreply.github.com>
2022-03-22 11:05:29 +0100

    adding some ressources and graphical changes on main page

--- app/src/main/java/com/example/minstalesapp/MainActivity.kt
+++ app/src/main/java/com/example/minstalesapp/MainActivity.kt
@@ -7,34 +7,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var loginButtonToggle: Boolean = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        playButton.setOnClickListener {
-            startActivity(Intent(this, ListStoryActivity::class.java))
-        }
-
-        profileButton.setOnClickListener {
-            startActivity(Intent(this, ProfileActivity::class.java))
-        }
-
-        marketButton.setOnClickListener {
-            startActivity(Intent(this, MarketplaceActivity::class.java))
-        }
-
-        micButton.setOnClickListener {
-            startActivity(Intent(this, RecordActivity::class.java))
-        }
-
-        loginButton.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
-        }
-
-        signUpButton.setOnClickListener {
-            startActivity(Intent(this, SignupActivity::class.java))
-        }
+        stories_toggle_button.isChecked = true
     }
 }
