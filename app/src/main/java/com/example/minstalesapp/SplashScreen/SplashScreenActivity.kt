e6e81ebb0a49d7921caef442ea561116377083a6
Jérémy Poisson <89804917+JeremyPoisson2B@users.noreply.github.com>
2022-03-23 16:35:44 +0100

    adding splash screen

---
+++ app/src/main/java/com/example/minstalesapp/SplashScreen/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.minstalesapp.SplashScreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.minstalesapp.MainPage.MainActivity
+import com.example.minstalesapp.databinding.ActivitySplashScreenBinding
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }, 3000)
+    }
+}
