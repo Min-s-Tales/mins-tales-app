b24d61a883678dedc5ecff4682308cc34717aa5a
Theovady Moutty <theomoutty@gmail.com>
2022-07-13 09:46:13 +0200

    refacto code

--- app/src/main/java/com/example/minstalesapp/SplashScreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/minstalesapp/SplashScreen/SplashScreenActivity.kt
@@ -1,18 +1,8 @@
 package com.example.minstalesapp.SplashScreen
 
-import android.app.DownloadManager
-import android.app.DownloadManager.Request.VISIBILITY_HIDDEN
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
-import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
-import android.webkit.CookieManager
-import android.webkit.URLUtil
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.minstalesapp.MainPage.MainActivity
 import com.example.minstalesapp.databinding.ActivitySplashScreenBinding
@@ -20,8 +10,6 @@ import com.example.minstalesapp.databinding.ActivitySplashScreenBinding
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
